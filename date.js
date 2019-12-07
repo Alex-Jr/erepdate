e226eb80dcd83116218099ae265a4328813ab925
Alex-Jr <jr.alexmendes@gmail.com>
2019-12-07 09:14:16 -0300

    Atualiza a data se mudar o topico do news feed

--- date.js
+++ date.js
@@ -5,15 +5,14 @@ window.onload = function(){
 
     c_header.innerText = addDays(initdate, parseInt(c_header.innerText.substring(4).replace(',','')))    
     
-    feed_dates = document.getElementsByClassName('time_posted');
-    for (i = 0; i < feed_dates.length; i++){
-        days = parseInt(feed_dates[i].innerText.substr(4).replace(',',''));
-        if (days !== days){
-            continue
-        }
-        feed_dates[i].innerText = addDays(initdate, days);
+    feedDate()
+    switchers = document.getElementsByClassName('switchers')
+    for (i = 0; i < switchers.length; i++){
+        switchers[i].addEventListener("click", function(){
+            feedDate()
+        });
     }
-
+        
     article_dates = document.getElementsByClassName('date');
     for (i = 1; i < article_dates.length; i++){
         days = parseInt(article_dates[i].innerText.substr(4).replace(',',''));
@@ -29,4 +28,17 @@ function addDays(date, days) {
     result.setDate(result.getDate() + days);
     n_date = result.getDate().toString() +"/"+ (result.getMonth()+1).toString() +"/"+ result.getFullYear().toString()
     return n_date
+}
+
+function feedDate(){
+    setTimeout(function(){
+        feed_dates = document.getElementsByClassName('time_posted');
+        for (i = 0; i < feed_dates.length; i++){
+            days = parseInt(feed_dates[i].innerText.substr(4).replace(',',''));
+            if (days !== days){
+                continue
+            }
+        feed_dates[i].innerText = addDays(initdate, days);
+    }
+    }, 3000);   
 }
