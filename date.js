bb51d593f6e047de97849e577f84116ed22dbadc
Alex-Jr <jr.alexmendes@gmail.com>
2019-12-07 08:53:44 -0300

    Fix na data com valor 0
    Date 0 fix

--- date.js
+++ date.js
@@ -1,7 +1,6 @@
+const initdate = new Date('11/20/2007');
 
 window.onload = function(){
-    const initdate = new Date('11/20/2007');
-
     c_header = document.getElementsByClassName('eday')[0];
 
     c_header.innerText = addDays(initdate, parseInt(c_header.innerText.substring(4).replace(',','')))    
@@ -28,7 +27,6 @@ window.onload = function(){
 function addDays(date, days) {
     var result = new Date(date);
     result.setDate(result.getDate() + days);
-    
-    n_date = result.getDay().toString() +"/"+ result.getMonth().toString() +"/"+ result.getFullYear().toString()
+    n_date = result.getDate().toString() +"/"+ (result.getMonth()+1).toString() +"/"+ result.getFullYear().toString()
     return n_date
 }
