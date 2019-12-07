ae63e4f33eb92debdb764b15e0beb6820d809454
Alex-Jr <jr.alexmendes@gmail.com>
2019-12-07 08:13:46 -0300

    Altera a data no caberário
    change date on header

---
+++ date.js
@@ -0,0 +1,14 @@
+const initdate = new Date('11/20/2007');
+
+c_header = document.getElementsByClassName('eday')[0];
+
+c_header.innerText = addDays(initdate, parseInt(c_header.innerText.substring(4).replace(',','')))    
+
+
+function addDays(date, days) {
+    var result = new Date(date);
+    result.setDate(result.getDate() + days);
+    
+    n_date = result.getDay().toString() +"/"+ result.getMonth().toString() +"/"+ result.getFullYear().toString()
+    return n_date
+}
