9cade393704fb934805fac2edd6879edbbf1ddc8
Alex-Jr <jr.alexmendes@gmail.com>
2019-12-07 08:16:49 -0300

    Altera data do mural de notícias
    Change date on the news feed

--- date.js
+++ date.js
@@ -5,7 +5,12 @@ window.onload = function(){
     c_header = document.getElementsByClassName('eday')[0];
 
     c_header.innerText = addDays(initdate, parseInt(c_header.innerText.substring(4).replace(',','')))    
-
+    
+    feed_dates = document.getElementsByClassName('time_posted');
+    for (i = 0; i < feed_dates.length; i++){
+        days = parseInt(feed_dates[i].innerText.substr(4).replace(',',''));
+        feed_dates[i].innerText = addDays(initdate, days);
+    }
 };
 
 function addDays(date, days) {
